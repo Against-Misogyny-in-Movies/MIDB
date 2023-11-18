4900bfaeb30d0904a424a95c1c17d325d439456a
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2023-11-18 15:59:02 +0100

    add more styles to tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,31 +6,46 @@ export default {
 
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'primary': {
+        light: colors.purple[300],
+        DEFAULT: colors.purple[500],
+        dark: colors.purple[700]
+      },
+      'secondary': {
+        light: '#E2E2D5',
+        DEFAULT: '#888883',
+        dark: '#55554E'
+      },
+      'neutral': {
+        light: colors.zinc[300],
+        DEFAULT: colors.zinc[500],
+        dark: colors.zinc[700]
+      },
+      white: colors.white,
+      black: colors.black,
+      gray: {
+        light: colors.slate[100],
+        DEFAULT: colors.slate[500],
+        dark: colors.slate[900]
+      }
+    },
+    'spacing': {
+      0: '0',
+      xs: '0.25rem',
+      sm: '0.5rem',
+      md: '1rem',
+      lg: '2rem',
+      xl: '3rem',
+    },
     extend: {
-      colors: {
-        'primary': {
-          light: colors.purple[300],
-          DEFAULT: colors.purple[500],
-          dark: colors.purple[700]
-        },
-        'secondary': {
-          light: '#E2E2D5',
-          DEFAULT: '#888883',
-          dark: '#55554E'
-        },
-        'neutral': {
-          light: colors.zinc[300],
-          DEFAULT: colors.zinc[500],
-          dark: colors.zinc[700]
-        },
-      },
-      'spacing': {
-        xs: '0.25rem',
-        sm: '0.5rem',
-        md: '1rem',
-        lg: '2rem',
-        xl: '3rem',
-      },
+     
     },
   },
   plugins: [],
