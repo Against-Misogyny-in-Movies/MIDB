a87a02c2adeb848331cee33acfcc08722a9777b7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-02-17 14:56:02 +0100

    remove @auth

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async (event) => {
-  const session = await event.locals.getSession();
-  if (!session?.user) throw redirect(303, '/auth/signin');
+export const load: PageServerLoad = async () => {
   return {};
 };
