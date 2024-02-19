900fa382a8ce8d7c69694da33bcd42a090c6dbee
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-02-19 13:37:07 +0100

    add simple navbar for navigation on page

---
+++ src/lib/components/navigation/item.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let href: string;
+    export let selected: boolean = false;
+</script>
+
+<li class:selected>
+    {#if selected}
+        <span>• <slot /></span>
+    {:else}
+        <a {href}>
+            <slot />
+        </a>
+    {/if}
+</li>
+
+<style lang="postcss">
+    li {
+        @apply font-bold p-sm;
+    }
+    li:hover {
+        @apply text-primary;
+    }
+
+    li.selected {
+        @apply text-primary;
+    }
+</style>
